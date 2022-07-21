dadfc1759d43d81a5363a61329f45772b271cb2b
SergeyZhilin93 <solomonsdt93@gmail.com>
2022-07-21 19:13:25 +0300

    add header for project

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./style.css"
+
+export function Header() {
+  return(
+    <>
+      <header className="header-container">
+        <p className="header-container-logo">My Musik</p>
+      </header>
+    </>
+  )
+}
