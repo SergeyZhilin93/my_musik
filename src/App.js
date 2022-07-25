b860ea5a10854b51b6ab8bf66ed2b5565bf16cf1
Sergey Zhilin <solomonsdt93@gmail.com>
2022-07-25 19:56:06 +0300

    add folders pages and AddPages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react"
 import { Header } from './components/Header'
+import { AddPage } from './pages/AddPage'
 
 function App() {
   return (
     <>
       <Header/>
+      <AddPage/>
     </>
   );
 }
