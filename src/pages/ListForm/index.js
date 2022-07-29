8d6b72b098b0a61e4121151a803cea65c7f57e9c
Sergey Zhilin <solomonsdt93@gmail.com>
2022-07-29 17:44:24 +0300

    add function show author and song name

--- src/pages/ListForm/index.js
+++ src/pages/ListForm/index.js
@@ -2,28 +2,34 @@ import React from 'react'
 import './style.css'
 
 export function ListPage() {
-	let firstSong = {
-		author: 'Tiesto',
-		songName: 'Business',
+	const tracks =[
+		{
+			author: 'Tiesto',
+			songName: 'Business',
+		},
+		{
+			author: 'T-Mass',
+			songName: 'Can you feel it',
+		},
+		{
+			author: 'F.O.O.L',
+			songName: 'Guns',
+		}
+	]
+	const test = () => {
+		for(let track of tracks) {
+			console.log(`${track.author} - ${track.songName}`)
+		}
 	}
-	let secondSong = {
-		author: 'T-Mass',
-		songName: 'Fool',
-	}
-	let trithSong = {
-		author: 'F.O.O.L',
-		songName: 'Guns',
-	}
-	// const test = () => {
-	// 	console.log('song:', `${user.author} - ${user.songName}`)
-	// }
+	
 	return(
 		<>
 			<div className='list-page-container'>
+				<button onClick={test}>TEST</button>
 				<h1>List Songs:</h1>
-				<p className='list-page-container__song'>{firstSong.author} - {firstSong.songName}</p>
-				<p className='list-page-container__song'>{secondSong.author} - {secondSong.songName}</p>
-				<p className='list-page-container__song'>{trithSong.author} - {trithSong.songName}</p>
+				{/* <p className='list-page-container__song'>{firstSong.author} - {firstSong.songName}</p>
+				<p className='list-pacontainer__songge-'>{secondSong.author} - {secondSong.songName}</p>
+				<p className='list-page-container__song'>{trithSong.author} - {trithSong.songName}</p> */}
 			</div>
 		</>
 	)
